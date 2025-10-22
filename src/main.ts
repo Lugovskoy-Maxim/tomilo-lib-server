7896500aa2893d67764250d6edaaa172927be620
Максим Луговской <lugovskou.myu@yandex.ru>
2025-10-22 15:13:36 +0500

    up: added global prefix api

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.setGlobalPrefix('api');
+
   // Включаем CORS
   app.enableCors();
 
