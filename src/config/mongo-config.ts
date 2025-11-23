dc1a7abbd3d481a5addb2dd9f40c63e118de98a1
Maxim Lugovskoy <lugovskou.myu@yandex.ru>
2025-11-23 23:52:42 +0500

    Update mongo-config.ts

--- src/config/mongo-config.ts
+++ src/config/mongo-config.ts
@@ -1,12 +1,22 @@
+import { ConfigService } from '@nestjs/config';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
+
 export const getMongoConfig = (
   configService: ConfigService,
 ): MongooseModuleOptions => {
-  const login = encodeURIComponent(configService.get('MONGO_LOGIN'));
-  const password = encodeURIComponent(configService.get('MONGO_PASSWORD'));
-  
-  const uri = `mongodb://${login}:${password}@${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}/${configService.get('MONGO_DATABASE')}?authSource=admin`;
+  const login = configService.get('MONGO_LOGIN');
+  const password = configService.get('MONGO_PASSWORD');
+  const host = configService.get('MONGO_HOST');
+  const port = configService.get('MONGO_PORT');
+  const database = configService.get('MONGO_DATABASE');
+  const authDatabase = configService.get('MONGO_AUTHDATABASE');
+
+  const uri = `mongodb://${login}:${password}@${host}:${port}/${database}?authSource=${authDatabase}`;
 
   console.log('Connecting to MongoDB...');
+  console.log(`Host: ${host}:${port}`);
+  console.log(`Database: ${database}`);
+  console.log(`Auth DB: ${authDatabase}`);
 
   return { 
     uri,
